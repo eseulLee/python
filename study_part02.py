006c12d79ce3bff60d7258af9ea48fae172ae422
Eseul Lee <dltmf3401@gmail.com>
2022-05-23 23:59:15 +0900

    빅분기 실기 준비 01

---
+++ study_part02.py
@@ -0,0 +1,19 @@
+### tuple
+'''
+1. 소괄호() 사용
+2. 데이터 수정, 추가 삭제 불가
+3. indexing O
+'''
+A = ('aa', 'bb', 'cc', 'dd')
+print(A)
+print(type(A))
+print('indexing - ', A[1], A[-3])
+
+### list
+'''
+1. 대괄호[] 사용
+2. 데이터 수정, 추가 삭제 가능
+'''
+A01 = ['aa', 'bb', 'cc', 'dd']
+print(A01)
+print(type(A01))
